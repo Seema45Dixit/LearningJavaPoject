5698cb2249c8294952a26ac3623347c54941b890
DIXITSEEMA-QA-LT <seema45dixitsharma@gmail.com>
2025-11-05 13:13:06 +0530

    Lab_002TEST3

--- src/ex_001_javabasic/Lab_002Java.java
+++
@@ -1,4 +0,0 @@
-package ex_001_javabasic;
-
-public class Lab_002Java {
-}
---
+++ src/ex_001_javabasic/Lab_003Java.java
@@ -0,0 +1,7 @@
+package ex_001_javabasic;
+
+public class Lab_003Java {
+    public static void main(String[] args) {
+        System.out.println("Seema Dixit");
+
+}}
