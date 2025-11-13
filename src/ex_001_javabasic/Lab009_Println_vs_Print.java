de71b3b8ca96b7b698ca248d3e61c0d5ba52e92e
DIXITSEEMA-QA-LT <seema45dixitsharma@gmail.com>
2025-11-13 19:27:45 +0530

    Lab009_Println_vs_Print

---
+++ src/ex_001_javabasic/Lab009_Println_vs_Print.java
@@ -0,0 +1,14 @@
+package ex_001_javabasic;
+
+public class Lab009_Println_vs_Print {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hi");
+        System.out.println("Bye");
+        System.out.print("Hi ");
+        System.out.print("Bye ");
+        System.out.print("seeyou");
+    }
+}
