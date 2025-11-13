8ac23309d772bb6d0efb58b2df84cdf5b92a518b
DIXITSEEMA-QA-LT <seema45dixitsharma@gmail.com>
2025-11-13 19:32:57 +0530

    Lab014_PrintF

---
+++ src/ex_002_Java/Lab014_PrintF.java
@@ -0,0 +1,19 @@
+package ex_002_Java;
+
+public class Lab014_PrintF {
+    public static void main(String[] args) {
+
+
+    int a = 10;
+    int b = 20;
+       System.out.println(a);
+        System.out.println(b);
+
+        System.out.print(a );
+        System.out.print(b );
+
+
+        System.out.printf("Value of a = %d", a);
+        System.out.println();
+        System.out.printf("Value of b = %d", b);
+}}
