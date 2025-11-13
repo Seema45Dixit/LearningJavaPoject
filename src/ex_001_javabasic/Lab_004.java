3cc48cf992f5324a367596e6fee76789c32f3894
DIXITSEEMA-QA-LT <seema45dixitsharma@gmail.com>
2025-11-13 19:23:14 +0530

    Lab_002TEST3

---
+++ src/ex_001_javabasic/Lab_004.java
@@ -0,0 +1,15 @@
+package ex_001_javabasic;
+
+public class Lab_004 {
+    public static void main(String[] args) {
+        System.out.println("seema ");
+
+
+        /*  comment multiple lines
+        int x=34;
+        int y=30;
+        System.out.println(x+y);
+        */
+
+    }
+}
